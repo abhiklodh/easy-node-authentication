f3027dcb27f4213c958ca0edc74828f5da46b45b
Abhik Lodh <8018018+abhiklodh@users.noreply.github.com>
2019-09-22 20:56:37 +0530

    Added Mongo DB Link

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_n3prnbtn:abhiklodh27@ds341847.mlab.com:41847/heroku_n3prnbtn' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
